245898cbcbe1741493f770a5baa8a4cb91f699d2
programmer-SS <167070988+programmer-SS@users.noreply.github.com>
2024-05-15 13:50:41 +0900

    renderで起動できるよう変更

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
-FROM node:20.11.0-slim
+FROM node:20.11.0
 
-RUN apt-get update && \ 
-    apt-get install -y locales vim tmux
+RUN apt-get update
+RUN apt-get install -y locales
+RUN apt-get install -y git
+RUN apt-get install -y procps
+RUN apt-get install -y vim
+RUN apt-get install -y tmux
+RUN apt-get install -y curl
 RUN locale-gen ja_JP.UTF-8
 RUN localedef -f UTF-8 -i ja_JP ja_JP
 ENV LANG=ja_JP.UTF-8
 ENV TZ=Asia/Tokyo
 WORKDIR /app
+COPY . /app
+CMD ["sh", "run.sh"]
--- index.js
+++ index.js
@@ -11,31 +11,45 @@ const server = http
 
     switch (req.method) {
       case 'GET':
-        if (req.url === '/enquetes/yaki-tofu') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '焼き肉',
-              secondItem: '湯豆腐'
-            })
-          );
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン'
-            })
-          );
-        } else if (req.url === '/enquetes/sushi-pizza') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '寿司',
-              secondItem: 'ピザ'
-            })
-          );
-        }
+        if (req.url === '/') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケートフォーム</h1>' +
+            '<a href="/enquetes">アンケート一覧</a>' +
+            '</body></html>');
+        } else if (req.url === '/enquetes') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケート一覧</h1><ul>' +
+            '<li><a href="/enquetes/yaki-tofu">焼き肉・湯豆腐</a></li>' +
+            '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+            '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+            '</ul></body></html>');
+        } else {
+          if (req.url === '/enquetes/yaki-tofu') {
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: '焼き肉',
+                secondItem: '湯豆腐'
+              })
+            );
+          } else if (req.url === '/enquetes/rice-bread') {
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: 'ごはん',
+                secondItem: 'パン'
+              })
+            );
+          } else if (req.url === '/enquetes/sushi-pizza') {
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: '寿司',
+                secondItem: 'ピザ'
+              })
+            );
+          };
+        };
         res.end();
         break;
       case 'POST':
@@ -64,7 +78,7 @@ const server = http
   .on('clientError', e => {
     console.error(`[${new Date()}] Client Error`, e);
   });
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.info(`[${new Date()}] Listening on ${port}`);
 });
